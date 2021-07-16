d191641ea184f1db54546d3636c9e499912080d8
m.hadeli <meysamhadeli@gmail.com>
2021-07-16 21:14:36 +0430

    .

--- University.Students/src/University.Students.Api/Program.cs
+++ University.Students/src/University.Students.Api/Program.cs
@@ -15,7 +15,6 @@ namespace University.Students.Api
         {
             CreateHostBuilder(args).Build().Run();
         }
-
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
